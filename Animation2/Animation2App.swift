f4ac93c82e0f960dd36d21c9216abd46c928579b
Mayur <mayurtyagi@unolo.com>
2024-07-23 14:09:15 +0530

    Initial Commit

---
+++ Animation2/Animation2App.swift
@@ -0,0 +1,17 @@
+//
+//  Animation2App.swift
+//  Animation2
+//
+//  Created by Mayur  on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Animation2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
